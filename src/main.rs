7a92120e92aede96e2a051c1bd2d437b879350ef
Matěj Volf <git@mvolfik.com>
2022-09-09 15:36:52 +0200

    Solve 02

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+#![feature(is_some_with)]
+#![feature(let_chains)]
 #![feature(let_else)]
+#![feature(linked_list_cursors)]
 #![feature(tcplistener_into_incoming)]
 mod p00;
 mod p01;
+mod p02;
 
 fn main() {
     let task: u8 = std::env::args().nth(1).unwrap().parse().unwrap();
     match task {
         0 => p00::main(),
         1 => p01::main(),
+        2 => p02::main(),
         _ => panic!("Invalid task number"),
     }
 }
---
+++ src/p02/mod.rs
@@ -0,0 +1,72 @@
+use std::{
+    collections::LinkedList,
+    io::{Read, Write},
+    net::{TcpListener},
+};
+
+
+pub fn main() {
+    let listener = TcpListener::bind("0.0.0.0:1200").unwrap();
+    for incoming in listener.into_incoming() {
+        let mut stream = match incoming {
+            Ok(stream) => stream,
+            Err(e) => {
+                eprintln!("Error accepting incoming stream: {:?}", e);
+                continue;
+            }
+        };
+        std::thread::spawn(move || {
+            let mut prices = LinkedList::<(u32, i32)>::new();
+            loop {
+                let mut bytes = [0; 9];
+                if let Err(e) = stream.read_exact(&mut bytes) {
+                    eprintln!("Error reading from stream: {:?}", e);
+                    stream.shutdown(std::net::Shutdown::Both).unwrap();
+                    break;
+                }
+                let (op_b, rest) = bytes.split_at(1);
+                let (num1_b, num2_b) = rest.split_at(4);
+                let op = op_b[0];
+                let num1 = u32::from_be_bytes(num1_b.try_into().unwrap());
+                let num2 = i32::from_be_bytes(num2_b.try_into().unwrap());
+
+                eprintln!("Received request: {} {} {}", op, num1, num2);
+                match op {
+                    b'I' => {
+                        let mut cursor = prices.cursor_front_mut();
+                        while cursor
+                            .current()
+                            .is_some_and(|(cursor_ts, _)| *cursor_ts < num1)
+                        {
+                            cursor.move_next();
+                        }
+                        cursor.insert_before((num1, num2));
+                        eprintln!("Current list state: {:?}", prices);
+                    }
+                    b'Q' => {
+                        let mut cursor = prices.cursor_front();
+                        while cursor
+                            .current()
+                            .is_some_and(|(cursor_ts, _)| *cursor_ts < num1)
+                        {
+                            cursor.move_next();
+                        }
+                        let mut n: u32 = 0;
+                        let mut sum = 0;
+                        while let Some((cursor_ts, cursor_value)) = cursor.current() && *cursor_ts <= num1 {
+                            n += 1;
+                            sum += cursor_value;
+                            cursor.move_next();
+                        };
+                        stream.write(&(sum / n as i32).to_be_bytes()).unwrap();
+                    }
+                    _ => {
+                        eprintln!("Invalid operation: {}", op);
+                        stream.shutdown(std::net::Shutdown::Both).unwrap();
+                        break;
+                    }
+                }
+            }
+        });
+    }
+}
